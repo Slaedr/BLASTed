76ede964c61bd000da510e0001ba4e56598298ff
Aditya Kashi <aditya.kashi@mail.mcgill.ca>
2017-11-12 16:53:50 -0500

    coo tests

--- tests/coomatrix.cpp
+++ tests/coomatrix.cpp
@@ -1,2 +1,12 @@
-/** 
+/** \file coomatrix.coo
+ * \brief Execution of unittests for coordinate matrix module
  */
+
+#include "testcoomatrix.hpp"
+
+int main(const int argc, const char *const argv[])
+{
+	int err = 0;
+
+	return err;
+}
