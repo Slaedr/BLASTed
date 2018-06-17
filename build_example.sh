d934d336210e4bbf8d34056b60ad59b44916f308
Aditya Kashi <aditya.kashi@mail.mcgill.ca>
2018-06-16 20:53:33 -0400

    changed the build example for build sub-directory in the BLASTed directory

--- build_example.sh
+++ build_example.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-# Create a build directory and run this file from there.
-# Needs BLASTED_DIR to be set to run.
-# Needs PETSC_DIR and PETSC_ARCH to be set.
+# In the BLASTed root directory, create a build directory called `build`
+# and run this file from there.
+# Needs EIGEN3_ROOT, PETSC_DIR and PETSC_ARCH to be set.
+# May also need BOOST_ROOT to be set, if Boost is installed in a non-standard location.
 # Assumes the compilers to use are mpicc and mpicxx.
 
 cmake -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DWITH_PETSC=1 \
-	  -DAVX=1 -DCMAKE_BUILD_TYPE=Release \
-	  ${BLASTED_DIR}
+	  -DAVX=1 -DCMAKE_BUILD_TYPE=Release ..
