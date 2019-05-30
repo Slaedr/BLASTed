1ae32a21e9ebfc27a28350dab7253c0aebf66199
Aditya Kashi <aditya.kashi@mail.mcgill.ca>
2019-05-30 15:01:34 -0400

    tested column adjacency

---
+++ tests/mat_ops/testcoladj.cpp
@@ -0,0 +1,43 @@
+/** \file
+ * \brief Test column adjacency computation
+ */
+
+#undef NDEBUG
+#include <stdexcept>
+#include "coomatrix.hpp"
+#include "../src/adjacency.hpp"
+
+using namespace blasted;
+
+int main(int argc, char *argv[])
+{
+	if(argc < 2)
+		throw std::runtime_error("Need matrix file!");
+
+	const std::string mfile = argv[1];
+
+	COOMatrix<double,int> coomat;
+	coomat.readMatrixMarket(mfile);
+	RawBSRMatrix<double,int> rmat;
+	coomat.convertToCSR(&rmat);
+
+	const ColumnAdjacency<double,int> ca(reinterpret_cast<CRawBSRMatrix<double,int>&>(rmat));
+	const std::vector<int>& cnzrow = ca.col_nonzero_rows();
+	const std::vector<int>& cnzloc = ca.col_nonzero_locations();
+	const std::vector<int>& cptr = ca.col_pointers();
+
+	// test
+	for(int jcol = 0; jcol < rmat.nbrows; jcol++) {
+		for(int ii = cptr[jcol]; ii < cptr[jcol+1]; ii++)
+		{
+			const int rowind = cnzrow[ii];
+			const int pos = cnzloc[ii];
+			assert(rmat.bcolind[pos] == jcol);
+			assert(pos >= rmat.browptr[rowind]);
+			assert(pos < rmat.browptr[rowind+1]);
+		}
+	}
+
+	destroyRawBSRMatrix(rmat);
+	return 0;
+}
