f7bf7a75c42a70505ef40f4f32b74d8f22335f7e
Aditya Kashi <aditya.kashi@mail.mcgill.ca>
2019-07-11 12:57:22 -0400

    added stubs for unstruct SAI pattern test

---
+++ tests/testunstructuredsaipattern.cpp
@@ -0,0 +1,27 @@
+/** \file
+ * \brief Test SAI/ISAI pattern generation for a small unstructured grid
+ */
+
+CRawBSRMatrix<double,int> generate_small_unstructured_matrix()
+{
+}
+
+void test_sai(const bool fullsai)
+{
+}
+
+void test_sai_upper(const bool fullsai)
+{
+}
+
+void test_sai_lower(const bool fullsai)
+{
+}
+
+int main(int argv, char *argv[])
+{
+	test_sai(true);
+	test_sai(false);
+
+	return 0;
+}
